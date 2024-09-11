687bea006b884ba90ead04319b90ed3a4346f987
thiagoafcf <thiago.afcf@gmail.com>
2024-09-11 17:33:46 -0300

    Exemplificação de 3 métodos para coleta de dados.

--- Modulo 7/coletaDados/coleta_basica.py
+++ Modulo 7/coletaDados/coleta_basica.py
@@ -1,4 +1,15 @@
+import pandas
 import requests
+from bs4 import BeautifulSoup
 
+print('Request: ')
 response = requests.get('https://br.financas.yahoo.com/quote/%5EBVSP/history/?guccounter=1')
 print(response.text[:600])
+
+print('\nBeautifulSoup: ')
+soup = BeautifulSoup(response.text, 'html.parser')
+print(soup.prettify()[:1000])
+
+print('\npandas: ')
+url_dados = pandas.read_html('https://br.financas.yahoo.com/quote/%5EBVSP/history')
+print(url_dados[0].head(10))
